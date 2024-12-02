43935b2e4a0ad08692c22d71943cf7554f481acd
feden2906 <feden2906@gmail.com>
2024-12-02 21:30:23 +0200

    lesson-1

---
+++ helpers/helper.js
@@ -0,0 +1,9 @@
+console.log('____________ helper.js_____________');
+console.log(__dirname);
+console.log(__filename);
+
+const foo = () => {
+    console.log('WWW @@@')
+}
+
+module.exports = { foo }
