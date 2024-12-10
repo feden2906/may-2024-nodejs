659be7e34cf175a6e455685734722402f00de067
feden2906 <feden2906@gmail.com>
2024-12-10 20:38:00 +0200

    hw-3

--- main.js
+++ main.js
@@ -1,45 +1,98 @@
 const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+const {read, write} = require('./fs.service.js');
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-let users = [
-    {id: 1, name: 'Maksym', email: 'feden@gmail.com', password: 'qwe123'},
-    {id: 2, name: 'Alina', email: 'alindosik@gmail.com', password: 'ert345'},
-    {id: 3, name: 'Anna', email: 'ann43@gmail.com', password: 'ghj393'},
-    {id: 4, name: 'Tamara', email: 'tomochka23@gmail.com', password: 'afs787'},
-    {id: 5, name: 'Dima', email: 'taper@gmail.com', password: 'rtt443'},
-    {id: 6, name: 'Rita', email: 'torpeda@gmail.com', password: 'vcx344'},
-    {id: 7, name: 'Denis', email: 'denchik@gmail.com', password: 'sdf555'},
-    {id: 8, name: 'Sergey', email: 'BigBoss@gmail.com', password: 'ccc322'},
-    {id: 9, name: 'Angela', email: 'lala@gmail.com', password: 'cdd343'},
-    {id: 10, name: 'Irina', email: 'irka7@gmail.com', password: 'kkk222'},
-];
-
-app.get('/users', (req, res) => {
-    res.json(users);
-});
-app.post('/users', (req, res) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+app.get('/users', async (req, res) => {
+    try {
+        const users = await read();
+        res.json(users);
+    } catch (e) {
+        res.status(500).json(e.message);
     }
-    users.push(newUser)
-    res.status(201).json(newUser);
 });
-app.get('/users/:userId', (req, res) => {
-    const user = users.find(user => user.id === Number(req.params.userId));
-    res.json(user);
+app.post('/users', async (req, res) => {
+    try {
+        if (!req.body.name || req.body.name.length < 3) {
+            return res.status(400).json('Name is required and should be minimum 3 symbols');
+        }
+        if (!req.body.email || !req.body.email.includes('@') ) {
+            return res.status(400).json('Email is required');
+        }
+        if (!req.body.password || req.body.password.length < 8) {
+            return res.status(400).json('Password is required and should be minimum 8 symbols');
+        }
+        const users = await read();
+        const newUser = {
+            id: users.length ? users[users.length - 1].id + 1 : 1,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }
+        users.push(newUser);
+        await write(users);
+        res.status(201).json(newUser);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
 });
-app.delete('/users/:userId', (req, res) => {
-    users = users.filter(user => user.id !== Number(req.params.userId));
-    res.sendStatus(204);
+
+app.get('/users/:userId', async (req, res) => {
+    try {
+        const users = await read();
+        const user = users.find(user => user.id === Number(req.params.userId));
+        res.json(user);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
+});
+app.put('/users/:userId', async (req, res) => {
+    try {
+        if (!req.body.name || req.body.name.length < 3) {
+            return res.status(400).json('Name is required and should be minimum 3 symbols');
+        }
+        if (!req.body.email || !req.body.email.includes('@') ) {
+            return res.status(400).json('Email is required');
+        }
+        if (!req.body.password || req.body.password.length < 8) {
+            return res.status(400).json('Password is required and should be minimum 8 symbols');
+        }
+        const users = await read();
+        const index = users.findIndex(user => user.id === Number(req.params.userId));
+
+        if (index === -1) {
+            return res.status(404).json('User not found');
+        }
+        const user = users[index];
+        user.name = req.body.name;
+        user.email = req.body.email;
+        user.password = req.body.password;
+
+        await write(users);
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
+})
+app.delete('/users/:userId', async (req, res) => {
+    try {
+        const users = await read();
+        const index = users.findIndex(user => user.id === Number(req.params.userId));
+
+        if (index === -1) {
+            return res.status(404).json('User not found');
+        }
+        users.splice(index, 1);
+        await write(users);
+        res.sendStatus(204);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
 });
 
 
