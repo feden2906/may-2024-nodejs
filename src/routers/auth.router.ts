5677ba79c95237064555d9e56198700c2e329fdc
feden2906 <feden2906@gmail.com>
2024-12-19 00:32:33 +0200

    lesson-6

---
+++ src/routers/auth.router.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { authController } from "../controllers/auth.controller";
+import { commonMiddleware } from "../middlewares/common.middleware";
+import { UserValidator } from "../validators/user.validator";
+
+const router = Router();
+
+router.post(
+  "/sign-up",
+  commonMiddleware.validateBody(UserValidator.create),
+  authController.signUp,
+);
+router.post(
+  "/sign-in",
+  // commonMiddleware.validateBody(UserValidator.create),
+  authController.signIn,
+);
+
+// router.post("/refresh", authController.refresh);
+
+export const authRouter = router;
